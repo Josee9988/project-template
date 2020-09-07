794aefa131c8c3edf0ab1b03a975a092b0901e70
Josee9988 <jgracia9988@gmail.com>
2020-09-07 11:56:36 +0200

    bake the script look for all extension file types

--- EXECUTEME.sh
+++ EXECUTEME.sh
@@ -9,8 +9,8 @@ read -p "Is this data correct: username \"$NEW_USERNAME\", email: \"$NEW_EMAIL\"
 case "$choice" in
   y|Y )
   # replace the username and email
-  find .github/ -type f -name "*.md" -print0 | xargs -0 sed -i "s/Josee9988/${NEW_USERNAME}/g"
-  find .github/ -type f -name "*.md" -print0 | xargs -0 sed -i "s/jgracia9988@gmail.com/${NEW_EMAIL}/g"
+  find .github/ -type f -name "*" -print0 | xargs -0 sed -i "s/Josee9988/${NEW_USERNAME}/g"
+  find .github/ -type f -name "*" -print0 | xargs -0 sed -i "s/jgracia9988@gmail.com/${NEW_EMAIL}/g"
   ;;
   n|N ) exit 0;;
   * ) echo "Invalid option";;
