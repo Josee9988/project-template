86eef2896397af8c65c1b9b838f1f9b2dc02a3d2
Josee9988 <jgracia9988@gmail.com>
2020-09-07 16:29:25 +0200

    "shell script tested and working"

--- EXECUTEME.sh
+++ EXECUTEME.sh
@@ -11,6 +11,13 @@ case "$choice" in
   # replace the username and email
   find .github/ -type f -name "*" -print0 | xargs -0 sed -i "s/Josee9988/${NEW_USERNAME}/g"
   find .github/ -type f -name "*" -print0 | xargs -0 sed -i "s/jgracia9988@gmail.com/${NEW_EMAIL}/g"
+
+  # delete the README.md and create one with just a header
+  echo "# **${NEW_USERNAME}'s project**" > README.md
+  echo "<!-- Write your own README.md file. Build something amazing! -->" >> README.md
+
+  # self remove this script
+  rm -- "$0"
   ;;
   n|N ) exit 0;;
   * ) echo "Invalid option";;
