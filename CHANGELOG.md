7607a1b546f715558244fe3e7f7589a1a08214dd
Josee9988 <jgracia9988@gmail.com>
2021-02-23 11:51:20 +0100

    "1.0.3 check if the github directory exists and removed funding links"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,16 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 ---
 
+## [**1.0.3**] - 2021-02-23
+
+### Removed
+
+* Josee's funding links.
+
+### Added
+
+* Shell script now checks if the .github directory exists.
+
 ## [**1.0.2**] - 2020-09-08
 
 ### Added
--- EXECUTEME.sh
+++ EXECUTEME.sh
@@ -11,7 +11,7 @@
 #                then this script will auto remove itself, to leave the new project without this file (EXECUTEME.sh).
 # TITLE:         EXECUTEME.sh
 # AUTHOR:        Jose Gracia
-# VERSION:       1.0.0
+# VERSION:       1.0.3
 # NOTES:         This script will auto remove itself, and in case of wanting to run it again, the user must download
 #                it again or do a 'git stash' and revert the changes.
 # BASH_VERSION:  5.0.17(1)-release
@@ -25,6 +25,12 @@
 # get the date.
 ACTUAL_DATE=$(date '+%Y-%m-%d')
 
+### Check if a directory does not exist ###
+if [ ! -d ".github/" ]; then
+  echo "Directory .github/ DOES NOT EXIST."
+  exit 1 # die with error code 1
+fi
+
 # prompt for the username and email
 read -p "Enter your username (without '@'): " NEW_USERNAME
 read -p "Enter your email: " NEW_EMAIL
@@ -32,9 +38,9 @@ read -p "Enter the name of the project: " PROJECT_NAME
 read -p "Enter what is your project (program/extension/API/web/CLI tool/backend/frontend/scrapper/automation tool/etc): " PROJECT_TYPE
 
 # confirm that the data is correct
-read -p "Is this data correct: username \"$NEW_USERNAME\", email: \"$NEW_EMAIL\" (y/n)?" choice
+read -p "Is this data correct: username \"$NEW_USERNAME\", email: \"$NEW_EMAIL\", project name: \"$PROJECT_NAME\", of type: \"$PROJECT_TYPE\" (y/n)?" choice
 case "$choice" in
-  y|Y )
+y | Y)
   # replace the username and email
   find .github/ -type f -name "*" -print0 | xargs -0 sed -i "s/Josee9988/${NEW_USERNAME}/g"
   find .github/ -type f -name "*" -print0 | xargs -0 sed -i "s/jgracia9988@gmail.com/${NEW_EMAIL}/g"
@@ -43,21 +49,26 @@ case "$choice" in
   rm LICENSE
 
   # delete the README.md and create one with just a header
-  echo "# **${NEW_USERNAME}'s project**" > README.md
-  echo "<!-- Write your own README.md file. Build something amazing! -->" >> README.md
+  echo "# **${NEW_USERNAME}'s project**" >README.md
+  echo "<!-- Write your own README.md file. Build something amazing! -->" >>README.md
 
   # write the basic structure of the CHANGELOG.md
-  echo -e "# **Change Log** 📜📝\n" > CHANGELOG.md
-  echo -e "All notable changes to the \"**${PROJECT_NAME}**\" ${PROJECT_TYPE} will be documented in this file.\n" >> CHANGELOG.md
-  echo -e "The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).\n" >> CHANGELOG.md
-  echo -e "---\n\n## [**0.0.1**] - ${ACTUAL_DATE}\n\n### Added\n" >> CHANGELOG.md
-  echo -e "* The basic project structure of [josee9988's](https://github.com/Josee9988) [project template](https://github.com/Josee9988/project-template)." >> CHANGELOG.md
-  echo -e "* A basic \`.gitignore\` file ignoring some linux files and IDE trash files." >> CHANGELOG.md
-  echo -e "* A \`.github/\` folder with issue templates, code of conduct, a contributing guide, pull request template security advisory file, a funding and support file and an issue label bot file." >> CHANGELOG.md
+  echo -e "# **Change Log** 📜📝\n" >CHANGELOG.md
+  echo -e "All notable changes to the \"**${PROJECT_NAME}**\" ${PROJECT_TYPE} will be documented in this file.\n" >>CHANGELOG.md
+  echo -e "The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).\n" >>CHANGELOG.md
+  echo -e "---\n\n## [**0.0.1**] - ${ACTUAL_DATE}\n\n### Added\n" >>CHANGELOG.md
+  echo -e "* The basic project structure of [josee9988's](https://github.com/Josee9988) [project template](https://github.com/Josee9988/project-template)." >>CHANGELOG.md
+  echo -e "* A basic \`.gitignore\` file ignoring some linux files and IDE trash files." >>CHANGELOG.md
+  echo -e "* A \`.github/\` folder with issue templates, code of conduct, a contributing guide, pull request template security advisory file, a funding and support file and an issue label bot file." >>CHANGELOG.md
+
+  # remove jose's custom funding links
+  echo -e "# add your own funding links" >.gitub/FUNDING.yml
+
+  echo -e "Remember to review every file and customize it as you like.\nYou are ready to start your brand new awesome project."
 
   # self remove this script
   rm -- "$0"
   ;;
-  n|N ) exit 0;;
-  * ) echo "Invalid option";;
+n | N) exit 0 ;;
+*) echo "Invalid option" ;;
 esac
