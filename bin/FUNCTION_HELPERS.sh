12979b19023c3916eaf79e0795087efc40221c1f
Josee9988 <jgracia9988@gmail.com>
2021-04-08 08:57:55 +0200

    "improved and added multiple more sections to the user readme.md"

--- bin/FUNCTION_HELPERS.sh
+++ bin/FUNCTION_HELPERS.sh
@@ -3,53 +3,84 @@
 # SCRIPT WITH EXPORTED FUNCTIONS AND VARIABLES
 
 writeREADME() {
-    bash -c "NEW_USERNAME='NEW_USERNAME' PROJECT_NAME='PROJECT_NAME' PROJECT_TYPE='PROJECT_TYPE'; cat << EOF > README.md
-<!-- Write your README.md file. Build something amazing! Modify this template as you wish -->
+  bash -c "NEW_USERNAME='NEW_USERNAME' PROJECT_NAME='PROJECT_NAME' PROJECT_TYPE='PROJECT_TYPE'; cat << EOF > README.md
+<!-- Write your README.md file. Build something amazing! This README.md template can guide you to build your project documentation, but feel free to modify it as you wish -->
 # 🔥 **$NEW_USERNAME/$PROJECT_NAME**
 
-<p align=\"center\">
+<div align=\"center\">
   <a href=\"https://github.com/$NEW_USERNAME/$PROJECT_NAME/issues\">
     <img alt=\"Issues\" src=\"https://img.shields.io/github/issues/$NEW_USERNAME/$PROJECT_NAME?color=0088ff&style=for-the-badge&logo=github\"/>
   </a>
   <a href=\"https://github.com/$NEW_USERNAME/$PROJECT_NAME/pulls\">
     <img alt=\"GitHub pull requests\" src=\"https://img.shields.io/github/issues-pr/$NEW_USERNAME/$PROJECT_NAME?color=0088ff&style=for-the-badge&logo=github\"/>
   </a>
-</p>
+</div>
 
 ---
 
-# ⚡ **Getting started**
+## 🤔 **About the project**
 
-* <!-- ... [YOU AWESOME TEXT HERE :D] -->
-
----
-
-# 🚀 **Usage**
-
-* <!-- ... [YOU AWESOME TEXT HERE :D] -->
+* <!-- ... [WHY DID YOU CREATED THIS PROJECT?, MOTIVATION, PURPOSE, DESCRIPTION, OBJECTIVES, etc] -->
 
 ---
 
-# 📝 **Aditional notes**
+## ⚡ **Installation**
 
-* <!-- ... [YOU AWESOME TEXT HERE :D] -->
-
----
-
-# ⚖️ **License and Changelog**
-
-See the license in **[LICENSE](LICENSE)** file.
-
-Watch the changes in the **[CHANGELOG.md](CHANGELOG.md)** file.
+* <!-- ... [SHOW HOW YOUR PROJECT IS INSTALLED] -->
 
 ---
 
-<!-- You may delete this if you want to -->
-This project was generated using *[Josee9988/project-template](https://github.com/Josee9988/project-template)* 📚
+### 🚀 **Usage**
+
+* <!-- ... [SHOW HOW YOUR PROJECT IS USED] -->
 
 ---
 
-## 🎉 Was the $PROJECT_TYPE helpful? Help us raise these numbers up
+### 🌲 **Project tree**
+
+* <!-- ... [SHOW YOUR PROJECT TREE] -->
+
+---
+
+## 📝 **Additional notes**
+
+* <!-- ... [ADD ADDITIONAL NOTES] -->
+
+---
+
+## 📸 **Screenshots**
+
+<!-- ... [SOME DESCRIPTIVE IMAGES] -->
+
+---
+
+## 🍰 **Supporters and donators**
+
+We are currently looking for new donators to help and maintain this project! ❤️
+
+By donating, you will help the development of this project and, *you will be featured in this $PROJECT_NAME's README.md* so everyone can see your kindness and visit your content ⭐.
+
+<a href=\"https://github.com/sponsors/$NEW_USERNAME\"> <!-- MODIFY THIS LINK TO YOUR MAIN DONATING SITE IF YOU ARE NOT IN THE GITHUB SPONSORS PROGRAM -->
+  <img src=\"https://img.shields.io/badge/Sponsor-this%20$PROJECT_NAME-blue?logo=github-sponsors&style=for-the-badge&color=red\">
+</a>
+
+<!-- LINK TO YOUR DONATING PAGES HERE -->
+
+---
+
+## ⚖️📝 **License and Changelog**
+
+See the license in the '**[LICENSE](LICENSE)**' file.
+
+Watch the changes in the '**[CHANGELOG.md](CHANGELOG.md)**' file.
+
+---
+
+$PROJECT_NAME was generated from *[Josee9988/project-template](https://github.com/Josee9988/project-template)* 📚
+
+---
+
+## 🎉 Was the $PROJECT_TYPE helpful? Help us raise these numbers up!
 
 [![Github followers](https://img.shields.io/github/followers/$NEW_USERNAME.svg?style=social)](https://github.com/$NEW_USERNAME)
 [![Github stars](https://img.shields.io/github/stars/$NEW_USERNAME/$PROJECT_NAME.svg?style=social)](https://github.com/$NEW_USERNAME/$PROJECT_NAME/stargazers)
@@ -64,8 +95,8 @@ EOF"
 }
 
 writeCHANGELOG() {
-    ACTUAL_DATE=$(date '+%Y-%m-%d')
-    bash -c "PROJECT_NAME='PROJECT_NAME' PROJECT_TYPE='PROJECT_TYPE' ACTUAL_DATE='ACTUAL_DATE'; cat << EOF > CHANGELOG.md
+  ACTUAL_DATE=$(date '+%Y-%m-%d')
+  bash -c "PROJECT_NAME='PROJECT_NAME' PROJECT_TYPE='PROJECT_TYPE' ACTUAL_DATE='ACTUAL_DATE'; cat << EOF > CHANGELOG.md
 <!-- markdownlint-disable MD024-->
 # **Change Log** 📜📝
 
