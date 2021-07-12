47a84b3541941fc7f93dce6fd66b5eb9c34e8855
Josee9988 <jgracia9988@gmail.com>
2021-07-12 20:18:36 +0200

    "updated the setup script with the new argument parse"

--- tests/project_scaffolding_test.sh
+++ tests/project_scaffolding_test.sh
@@ -30,7 +30,7 @@ oneTimeSetUp() {
     rm -r $TESTS_TRASH_DIR/tests/ 2>/dev/null || :
     rm -r $TESTS_TRASH_DIR/.git/ 2>/dev/null || :
     cd $TESTS_TRASH_DIR || exit
-    bash SETUP_TEMPLATE.sh $USERNAME $NAME $MAIL $TYPE $OMIT_STR >/dev/null # run the setup script
+    bash SETUP_TEMPLATE.sh --username=$USERNAME --projectName=$NAME --email=$MAIL --projectType=$TYPE $OMIT_STR >/dev/null # run the setup script
 }
 
 oneTimeTearDown() {
